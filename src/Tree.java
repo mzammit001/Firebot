78ba7de655034dd94b3929182a2b7d1f0a697fb7
mzammit001 <mzam7159@uni.sydney.edu.au>
2016-05-26 23:25:43 +1000

    Finished up the tree class for now

--- src/Tree.java
+++ src/Tree.java
@@ -8,9 +8,6 @@ public class Tree {
     private int height;
     private int intensity;
     private boolean burntDown;
-    //
-    // TODO
-    //
 
 	public Tree(int height) {
         this.height = height;
@@ -18,22 +15,73 @@ public class Tree {
         this.burntDown = false;
 	}
 
+    /**
+     * is the tree burnt down
+     * @return
+     */
     public boolean isBurntDown() {
         return this.burntDown;
     }
 
+    /**
+     * get the current height
+     * @return
+     */
     public int getHeight() {
         return this.height;
     }
 
+    /**
+     * get the current fire intensity
+     * @return
+     */
     public int getIntensity() {
         return this.intensity;
     }
 
+    /**
+     * set the fire intensity
+     * @param intensity
+     */
     public void setIntensity(int intensity) {
         this.intensity = intensity;
     }
 
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void tryBurn() {
+        if (getIntensity() == 9 && getHeight() > 0)
+            setHeight(getHeight()-1);
+        else if (getIntensity() > 0)
+            setIntensity(getIntensity()+1);
+        else
+            return;
+
+        // all we got left is embers, farewell tree
+        if (getIntensity() == 9 && getHeight() == 0) {
+            this.burntDown = true;
+            setIntensity(0);
+        }
+    }
+
+    /**
+     * get the trees contribution to pollution
+     * @return
+     */
+    public int getPollution() {
+        int p = 0;
+        p -= getHeight();
+        p += getIntensity();
+
+        return p;
+    }
+
+    /**
+     * get a char value that we can print for height
+     * @return
+     */
     public char getPrintableHeight() {
         // display 'x' if burnt down
         if (isBurntDown())
@@ -44,6 +92,10 @@ public class Tree {
         return (getHeight() == 0) ? ' ' : (char)(0x30+getHeight());
     }
 
+    /**
+     * get a char value we can print for fire intensity
+     * @return
+     */
     public char getPrintableIntensity() {
         // display 'x' if burnt down
         if (isBurntDown())
